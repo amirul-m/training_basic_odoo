23863eaa73b566ea486713301f29cde1cbcb7736
Amirul <muh.amirul.muk@gmail.com>
2024-01-13 16:49:31 +0700

    training_day1

---
+++ training_day1/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Training 17',
+    'version' : '1.2',
+    'summary': 'Day 1',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': '',
+    'website': 'https://www.arkana.co.id',
+    'depends': ['base'],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
