dd6244ee05fa336c4f905b0ef940e3e15f64e015
Amirul <muh.amirul.muk@gmail.com>
2024-01-20 10:13:41 +0700

    [ADD] training_day3

---
+++ training_day3/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : 'Training 17 Day 3',
+    'version' : '1.2',
+    'summary': 'Day 3',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': '',
+    'website': 'https://www.arkana.co.id',
+    'depends': ['training_day2'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
