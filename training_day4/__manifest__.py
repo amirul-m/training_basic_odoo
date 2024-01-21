d8f05766a09b4d9da275783bed0ab02cd81a77db
Amirul <muh.amirul.muk@gmail.com>
2024-01-21 13:03:43 +0700

    [ADD] training_day4

---
+++ training_day4/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'Training 17 Day 4',
+    'version' : '1.2',
+    'summary': 'Day 3',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': '',
+    'website': 'https://www.arkana.co.id',
+    'depends': ['training_day3', 'sale'],
+    'data': [
+        'data/data.xml',
+        'views/training_class_view.xml',
+        'report/report.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ training_day4/models/training_class.py
@@ -0,0 +1,29 @@
+from odoo import fields, models, api
+from odoo.exceptions import UserError
+
+
+class TrainingClass(models.Model):
+    _inherit = 'training.class'
+
+    name_seq = fields.Char(string='Number')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            # create sequence on save
+            vals['name_seq'] = self.env['ir.sequence'].next_by_code('training.class')
+
+        return super().create(vals_list)
+
+
+    def set_to_confirmed(self):
+        res = super().set_to_confirmed()
+        if not self.name_seq:
+            self.name_seq = self.env['ir.sequence'].next_by_code('training.class')
+        return res
+    
+    def action_view_member_ids(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('training_day4.training_member_action')
+        action["domain"] = [("class_id", "=", self.id)]
+        return action
+
